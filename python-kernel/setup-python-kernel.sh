feed4e7beb3bee2540c47ace7b141c29c4ea6adc
voigta80 (Aiko Voigt) <aiko.voigt@univie.ac.at>
2024-10-17 00:47:09 +0200

    Adds python kernel for srvx1 and first era5 download script

--- python-kernel/setup-python-kernel.sh
+++ python-kernel/setup-python-kernel.sh
@@ -1,19 +1,20 @@
+# note: 
+# 1. cdsapi requires cgi, which was removed from python in python3.11; I am therefore specifying a lower python version
+# 2. I also found that I need to use numpy1 and not the new numpy2, which came out in summer 2024
+
 # move to SRV home directory
 cd /srvfs/home/avoigt
 
-KNAME="intro-comp-meteo-ex-w2024"  # name of pyhton kernel
+KNAME="intro-comp-meteo-ex-w2024"  # name of python kernel
 ROOTPREFIX="/srvfs/home/avoigt/micromamba"
 
 module load micromamba
 micromamba create -n $KNAME -r $ROOTPREFIX
-micromamba install -c conda-forge -n $KNAME -r $ROOTPREFIX xarray \ pandas \ numpy \ matplotlib \ cartopy \ netcdf4
-micromamba install -c conda-forge -n $KNAME -r $ROOTPREFIX dask \ zarr 
-micromamba install -c conda-forge -n $KNAME -r $ROOTPREFIX cdsapi
+micromamba install -c conda-forge -n $KNAME -r $ROOTPREFIX python=3.10 \ pandas \ numpy=1.26.4 \ matplotlib \ cartopy \ netcdf4 \ xarray \ dask \ zarr \ cdsapi \ ipykernel
 
 # create Jupyter kernel and make available
-micromamba install -c conda-forge -n $KNAME -r $ROOTPREFIX ipykernel
 ${ROOTPREFIX}/envs/${KNAME}/bin/python3 -m ipykernel install --user --name=$KNAME
 
-# export evironment setup to env.txt
+# export environment setup to env.txt
 micromamba env export -p ${ROOTPREFIX}/envs/${KNAME}/ > env.txt
 
