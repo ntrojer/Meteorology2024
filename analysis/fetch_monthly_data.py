aff7c4a3e3da0c5f084af7d001ec8b8b4825d3da
Julia Fiorino <juliafiorino20@gmail.com>
2024-11-26 11:10:19 +0000

    Fetch monthly data, store it in a folder, and generate plots. Would be more efficient to store everything in one netcdf

--- analysis/fetch_monthly_data.py
+++ analysis/fetch_monthly_data.py
@@ -3,16 +3,27 @@ import core as core
 from pathlib import Path
 
 
-def fetch_monthly_data(path, new_data):
-    if new_data:
-        Path("era5-1950-monthly").mkdir(parents=True, exist_ok=True)
-    
-        ds2=xr.open_mfdataset(path + "era5-1950-01.nc", chunks={"valid_time":1e5} )
-        ds2["wspd"] = core.windspeed(ds2)
-        ds2["pvpot2"] = core.pv_pot(ds2).groupby(ds2.valid_time.dt.month).mean("valid_time").compute()
-        # pvpot2 = core.pv_pot(ds2).groupby(ds2.valid_time.dt.month).mean("valid_time").compute()
-        ds2[["pvpot2", "longitude", "latitude"]].to_netcdf("era5-1950-monthly/era5-1950-01.nc")
-    else:
-        ds2 = xr.open_dataset("era5-1950-monthly/era5-1950-01.nc")
+def fetch_monthly_data(path, new_data, years):
+    months = [f"{month:02}" for month in range(1, 13)]
+    ds2_dict = {}
 
-    return ds2
+    # TODO: make this handle a list of years
+    year=years[0] 
+    
+    if new_data:
+        Path(f"era5-{year}-monthly").mkdir(parents=True, exist_ok=True)
+        for month in months:
+            ds2=xr.open_mfdataset(path + f"era5-{year}-{month}.nc", chunks={"valid_time":1e5} )
+            ds2["wspd"] = core.windspeed(ds2)
+            ds2["pvpot2"] = core.pv_pot(ds2).groupby(ds2.valid_time.dt.month).mean("valid_time").compute()
+            # pvpot2 = core.pv_pot(ds2).groupby(ds2.valid_time.dt.month).mean("valid_time").compute()
+            ds2[["pvpot2", "longitude", "latitude"]].to_netcdf(f"era5-{year}-monthly/era5-{year}-{month}.nc")
+            
+            ds2_dict[f"{year}-{month}"] = ds2
+    else:
+        for month in months:
+            ds2 = xr.open_dataset(f"era5-{year}-monthly/era5-{year}-{month}.nc")
+
+            ds2_dict[f"{year}-{month}"] = ds2
+
+    return ds2_dict
--- analysis/main.py
+++ analysis/main.py
@@ -13,43 +13,78 @@ import fetch_yearly_data
 
 warnings.filterwarnings("ignore")
 
+def save_fig(ds2_dict):
+    for key, ds2 in ds2_dict.items():
+        fig, ax = plt.subplots(figsize=(10, 8), subplot_kw={'projection': ccrs.PlateCarree()})
+
+        # Main plot: Contour plot of potential vorticity
+        contour = ax.contourf(ds2.longitude, ds2.latitude, ds2.pvpot2[0, :, :], cmap="viridis")
+        fig.colorbar(contour, ax=ax, orientation="vertical", label="Potential Vorticity")
+        
+        ax.coastlines(color='white', linewidth=0.7)
+        ax.add_feature(cfeature.BORDERS, edgecolor='white', linewidth=0.5)
+        
+        gl = ax.gridlines(draw_labels=True, linestyle="None")
+        gl.top_labels = False  # Disable top labels
+        gl.right_labels = False  # Disable right labels
+        gl.xlabel_style = {'size': 10, 'color': 'black'}
+        gl.ylabel_style = {'size': 10, 'color': 'black'}
+
+        plt.show()
+        
+        plt.savefig(f"monthly_plots/pvpot-{key}.png")
+
 # location of era5 data on teachinghub
 path="../../LEHRE/msc-intro-comp-met-ex-w2024/data/era5/"
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--new_data", action="store_true", help="If you want to save time fetching the data, set this to false")
+# USAGE: python main.py --new_data monthly 1950
+# USAGE: python main.py monthly 1950
+parser.add_argument("--new_data", action="store_true", help="If you include --new_data, this will be true. If you omit it, it will be false")
+parser.add_argument("timestep", type=str, choices=["monthly", "yearly"], help="Select the timestep, either 'monthly' or 'yearly'")
+parser.add_argument("start_year", type=int, help="Specify the start year")
+parser.add_argument("--end_year", type=int, help="Select the end year (optional). If no end year provided, only uses the start year")
+
 args = parser.parse_args()
 
 # TODO: change these two lines in arguments that allows you to set the date range
 monthly = True # change this to False if you want yearly data
-years= ["1950"]
+
+years = []
+if args.end_year:
+    years=list(range(args.start_year, args.end_year + 1))
+else:
+    years=[args.start_year]
 
 
 if monthly:
-    ds2 = fetch_monthly_data.fetch_monthly_data(path=path, new_data=args.new_data)
+    ds2_dict = fetch_monthly_data.fetch_monthly_data(path=path, new_data=args.new_data, years=years)
     Path("monthly_plots").mkdir(parents=True, exist_ok=True)
+    save_fig(ds2_dict)
 else:
-    # uncomment this if you want to fetch monthly data
+    # THE YEARLY DATA DOESNT WORK YET! JUST WORK WITH MONTHLY DATA FOR NOW
     ds2=fetch_yearly_data.get_filelists(years,new_data=args.new_data)
     Path("yearly_plots").mkdir(parents=True, exist_ok=True)
+    save_fig(ds2)
 
-fig, ax = plt.subplots(figsize=(10, 8), subplot_kw={'projection': ccrs.PlateCarree()})
 
-# Main plot: Contour plot of potential vorticity
-contour = ax.contourf(ds2.longitude, ds2.latitude, ds2.pvpot2[0, :, :], cmap="viridis")
-fig.colorbar(contour, ax=ax, orientation="vertical", label="Potential Vorticity")
+# fig, ax = plt.subplots(figsize=(10, 8), subplot_kw={'projection': ccrs.PlateCarree()})
 
-ax.coastlines(color='white', linewidth=0.7)
-ax.add_feature(cfeature.BORDERS, edgecolor='white', linewidth=0.5)
+# # Main plot: Contour plot of potential vorticity
+# contour = ax.contourf(ds2.longitude, ds2.latitude, ds2.pvpot2[0, :, :], cmap="viridis")
+# fig.colorbar(contour, ax=ax, orientation="vertical", label="Potential Vorticity")
 
-gl = ax.gridlines(draw_labels=True, linestyle="None")
-gl.top_labels = False  # Disable top labels
-gl.right_labels = False  # Disable right labels
-gl.xlabel_style = {'size': 10, 'color': 'black'}
-gl.ylabel_style = {'size': 10, 'color': 'black'}
+# ax.coastlines(color='white', linewidth=0.7)
+# ax.add_feature(cfeature.BORDERS, edgecolor='white', linewidth=0.5)
 
-plt.show()
-if monthly: 
-    plt.savefig('monthly_plots/pvpot-1950-01.png')
-else:
-    plt.savefig('monthly_plots/pvpot-1950.png')
+# gl = ax.gridlines(draw_labels=True, linestyle="None")
+# gl.top_labels = False  # Disable top labels
+# gl.right_labels = False  # Disable right labels
+# gl.xlabel_style = {'size': 10, 'color': 'black'}
+# gl.ylabel_style = {'size': 10, 'color': 'black'}
+
+# plt.show()
+# if monthly: 
+#     plt.savefig('monthly_plots/pvpot-1950-01.png')
+# else:
+#     plt.savefig('monthly_plots/pvpot-1950.png')
